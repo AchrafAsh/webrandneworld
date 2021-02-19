76d2ca79bea480fe3d54067e6addb0ca8174bf16
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-02-20 00:53:53 +0100

    Replace top header and add alt property to images

--- src/pages/yakel-mayers.jsx
+++ src/pages/yakel-mayers.jsx
@@ -1,26 +1,18 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Cover1 from '../images/cover_1.png'
 import Cover2 from '../images/cover_2.png'
 import Cover3 from '../images/cover_3.png'
+import TopTitle from '../components/TopTitle'
 
 export default () => (
     <Layout back>
         <div className='flex flex-col items-center'>
-            <div className='flex flex-row space-x-4 items-center text-2xl p-12'>
-                <Link to='/mazing-day'>
-                    <h1>Mazing Day</h1>
-                </Link>
-                <span>|</span>
-                <Link to='/yakel-mayers'>
-                    <h1>Yakel Mayers</h1>
-                </Link>
-            </div>
+            <TopTitle artist='yakel-mayers' />
             <div className='w-full flex flex-col items-center space-y-12 py-12'>
-                <img src={Cover1} />
-                <img src={Cover2} />
-                <img src={Cover3} />
+                <img src={Cover1} alt='' title='Cover' />
+                <img src={Cover2} alt='' title='Cover' />
+                <img src={Cover3} alt='' title='Cover' />
             </div>
         </div>
     </Layout>
