ececa35f4b7506294e5f2051ebfc1283bba21c0b
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-03-06 16:15:35 +0100

    Cleaning

--- src/components/TopTitle.jsx
+++ src/components/TopTitle.jsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 
 export default ({ artist }) => (
-    <div className='flex flex-row space-x-4 items-center text-2xl p-12'>
+    <div className='text-2xl px-6 pt-12'>
         {artist === 'yakel-mayers' ? (
             <Link to='/yakel-mayers'>
                 <h1>Yakel Mayers</h1>
@@ -14,24 +14,3 @@ export default ({ artist }) => (
         )}
     </div>
 )
-// <div className='flex flex-row space-x-4 items-center text-2xl p-12'>
-//     <Link to='/mazing-day'>
-//         <h1
-//             className={
-//                 artist === 'mazing-day' ? 'font-medium' : 'font-light'
-//             }
-//         >
-//             Mazing Day
-//         </h1>
-//     </Link>
-//     <span className='font-light text-gray-300'>|</span>
-//     <Link to='/yakel-mayers'>
-//         <h1
-//             className={
-//                 artist === 'yakel-mayers' ? 'font-medium' : 'font-light'
-//             }
-//         >
-//             Yakel Mayers
-//         </h1>
-//     </Link>
-// </div>
