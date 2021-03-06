77daccae2d7903a9765d71be8b7e3393572cbee3
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-03-06 16:16:26 +0100

    Responsive layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,22 +1,32 @@
 import * as React from 'react'
-import Sidebar from './sidebar'
+import { Topbar, Sidebar } from './sidebar'
 import SEO from './SEO'
 
 export default ({ title, back, children, hide }) => (
-    <>
+    <div className='min-h-screen flex flex-col'>
         <SEO title={title} />
-        <main className='flex flex-row min-h-screen w-full'>
-            {!hide && <Sidebar back={back} />}
-            <section className={`w-full ${hide ? '' : 'pl-24'}`}>
+        <main className='flex-1 flex flex-col-reverse sm:flex-row'>
+            {!hide && (
+                <>
+                    <div className='hidden sm:block'>
+                        <Sidebar back={back} />
+                    </div>
+                    <div className='sm:hidden'>
+                        <Topbar back={back} />
+                    </div>
+                </>
+            )}
+            <section className={`w-full ${hide ? '' : 'sm:pl-24'}`}>
+                {!hide && <div className='mb-12 sm:mb-0' />}
                 {children}
             </section>
         </main>
-        <footer className='w-full border-t border-gray-200 mt-24'>
-            <div className='max-w-5xl mx-auto py-12 text-gray-600'>
+        <footer className='w-full border-t border-gray-200'>
+            <div className='max-w-5xl mx-auto p-6 sm:p-12 text-gray-600'>
                 <small>
                     © {new Date().getFullYear()} - Mazing Day & Yakel Mayers
                 </small>
             </div>
         </footer>
-    </>
+    </div>
 )
