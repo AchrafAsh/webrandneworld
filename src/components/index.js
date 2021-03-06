6b99eaae768273c6f7f6df7423cf47576f3defc9
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-03-06 16:15:09 +0100

    Export CoverList component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as YoutubeEmbed } from './YoutubeEmbed'
 export { default as TopTitle } from './TopTitle'
 export { default as SEO } from './SEO'
+export { default as CoverList } from './CoverList'
