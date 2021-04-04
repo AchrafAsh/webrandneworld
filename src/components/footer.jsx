a108f8e8719489811280a25f41f6bfaf46ec7c23
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-04-04 17:00:44 +0200

    Remove top border

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default () => (
+    <footer className='w-full border-gray-200'>
+        <div className='max-w-4xl mx-auto p-6 sm:pl-24 sm:p-12 md:p-12 text-gray-600'>
+            <small>
+                © {new Date().getFullYear()} - Mazing Day & Yakel Mayers
+            </small>
+        </div>
+    </footer>
+)
