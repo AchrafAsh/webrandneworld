0bbb4c80761ff476c318782b8f668e4c46f160e3
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-04-04 17:00:31 +0200

    Custom 404 page (with auto redirect to home)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,38 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const useTimeout = (callback, delay) => {
+    const savedCallback = React.useRef()
+
+    React.useEffect(() => {
+        savedCallback.current = callback
+    }, [callback])
+
+    React.useEffect(() => {
+        function tick() {
+            savedCallback.current()
+        }
+        if (delay !== null) {
+            let id = setTimeout(tick, delay)
+            return () => clearTimeout(id)
+        }
+    }, [delay])
+}
 
 // markup
 const NotFoundPage = () => {
+    useTimeout(() => {
+        if (typeof window !== 'undefined') {
+            window.location = '/'
+        }
+    }, 3000)
     return (
-        <main>
-            <title>Not found</title>
-            <h1>Page not found</h1>
-            <p>
-                Sorry{' '}
-                <span role='img' aria-label='Pensive emoji'>
-                    😔
-                </span>{' '}
-                we couldn’t find what you were looking for.
-                <br />
-                <Link to='/'>Go home</Link>.
-            </p>
-        </main>
+        <Layout title='Ooops' hide>
+            <div className='h-full flex flex-col space-y-2 items-center justify-center'>
+                <h1 className='text-8xl font-black'>404</h1>
+                <h1 className='text-5xl font-black'>Page not found</h1>
+            </div>
+        </Layout>
     )
 }
 
