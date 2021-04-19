9af5a143e412d01d3db383471327698af6526803
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-04-19 08:22:01 +0200

    Handle different types of youtube links

--- src/components/YoutubeEmbed.jsx
+++ src/components/YoutubeEmbed.jsx
@@ -1,12 +1,32 @@
 import * as React from 'react'
 
-export default ({ link }) => (
-    <iframe
-        className='mx-auto w-full max-h-80 sm:max-h-screen'
-        height='512'
-        src={link}
-        frameBorder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowFullScreen
-    />
-)
+export default ({ link }) => {
+    // https://youtube.com/embed/{videoID}
+    let youtubeLink = ''
+    if (link.includes('embed')) youtubeLink = link
+    else {
+        if (link.includes('watch')) {
+            const videoID = link.substring(
+                link.indexOf('v=') + 2,
+                link.indexOf('&')
+            )
+            youtubeLink = `https://youtube.com/embed/${videoID}`
+        }
+        if (link.includes('youtu.be')) {
+            const videoID = link.split('/')[3]
+            youtubeLink = `https://youtube.com/embed/${videoID}`
+        }
+    }
+
+    return (
+        <iframe
+            className='mx-auto w-full max-h-80 sm:max-h-screen'
+            height='512'
+            title='Youtube Video'
+            src={youtubeLink}
+            frameBorder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+        />
+    )
+}
