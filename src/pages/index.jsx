702e21eb990e5aa46958408e75b238f004d44fb9
achrafash <aitsidihammou.achraf@gmail.com>
2021-11-13 19:38:55 +0100

    feat: add loading screen while loading thumbnails

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,25 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import ArtistCard from '../components/ArtistCard'
+import LoadingScreen from '../components/LoadingScreen'
 
 
 const IndexPage = () => {
+    const [loading, setLoading] = React.useState({ yakelMayers:true, mazingDay:true })
+
+    React.useEffect(() => {
+        const mazingDayThumbnail = new Image()
+        const yakelMayersThumbnail = new Image()
+
+        mazingDayThumbnail.onload = () => { setLoading(prev => ({...prev, mazingDay:false}))}
+        yakelMayersThumbnail.onload = () => { setLoading(prev => ({...prev, yakelMayers: false})) }
+
+        mazingDayThumbnail.src = 'mazing_day_thumbnail.png'
+        yakelMayersThumbnail.src = 'yakel_mayers_thumbnail.png'
+    })
+
+    if (loading.yakelMayers || loading.mazingDay) return <LoadingScreen />
+
     return (
         <Layout title='Home' hide>
             <div className='h-screen md:p-24 lg:max-w-8xl lg:mx-auto'>
